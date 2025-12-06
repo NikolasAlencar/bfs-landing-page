442c7cda61a1fedd4b36eb79e5330c6217b88db9
Nikolas Alencar <nikolasalencarrs73@gmail.com>
2025-12-05 22:24:10 -0300

    feat: adiciona icone no toggle

--- components/ThemeToggle/ThemeToggle.tsx
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useTheme } from "@/context/ThemeContext";
+import { FiSun, FiMoon } from "react-icons/fi";
 import "./theme-toggle.css";
 
 export function ThemeToggle() {
@@ -8,8 +9,19 @@ export function ThemeToggle() {
 
   return (
     <label className="switch">
-      <input type="checkbox" checked={theme === "dark"} onChange={toggleTheme} />
-      <span className="slider"></span>
+      <input
+        type="checkbox"
+        checked={theme === "dark"}
+        onChange={toggleTheme}
+      />
+      <span className="slider">
+        <span
+          className="icon"
+          style={theme === "light" ? { left: "0.5rem" } : undefined}
+        >
+          {theme === "dark" ? <FiMoon size={16} /> : <FiSun size={16} />}
+        </span>
+      </span>
     </label>
   );
 }
